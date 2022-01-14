f6bb991d3f3f9c0f62f714753e9af5a48dec4060
Agnes Leroy <agnes.leroy@zama.ai>
2022-01-14 16:08:34 +0100

    add task for core book

--- concrete-tasks/src/doc.rs
+++ concrete-tasks/src/doc.rs
@@ -5,3 +5,7 @@ pub fn test_book_boolean() -> Result<(), Error> {
     cmd!("cargo build -p concrete-boolean --release")?;
     cmd!("mdbook test concrete-boolean/book -L target/release/deps")
 }
+pub fn test_book_core() -> Result<(), Error> {
+    cmd!("cargo build -p concrete-core --release")?;
+    cmd!("mdbook test concrete-core/book -L target/release/deps")
+}
